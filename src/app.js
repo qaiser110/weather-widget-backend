45b7bd1eae059a17db937015ca54ecf9a39d779f
Qaiser Abbas <Abbas.Qaiser@abc.net.au>
2018-08-07 20:30:16 +1000

    Added cors

--- src/app.js
+++ src/app.js
@@ -1,5 +1,13 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
+
+app.use(
+  cors({
+    origin: '*',
+    methods: 'GET'
+  })
+);
 
 const weatherRouter = require('./router/weather');
 const locationRouter = require('./router/location');
